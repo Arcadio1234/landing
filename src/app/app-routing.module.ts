3be1072d7e1f634f838c618ed02a49c78a8ff0d7
Arcadio1234 <utm19040022@utma.edu.mx>
2022-07-08 12:46:36 -0500

    Proyecto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LuisComponent } from './component/luis/luis.component';
+import { MusicComponent } from './component/music/music.component';
+import { CrudComponent } from './pages/crud/crud.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+{path: "crud", component: CrudComponent},
+{path: "luis", component: LuisComponent},
+{path: "music", component: MusicComponent},
+
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CrudComponent } from './pages/crud/crud.component';
+import { RegistrarUsuarioComponent } from './pages/crud/registrar-usuario/registrar-usuario.component';
+import { ActualizarUsuarioComponent } from './pages/crud/actualizar-usuario/actualizar-usuario.component';
+import { LuisComponent } from './component/luis/luis.component';
+import { MusicComponent } from './component/music/music.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CrudComponent,
+    RegistrarUsuarioComponent,
+    ActualizarUsuarioComponent,
+    LuisComponent,
+    MusicComponent
   ],
   imports: [
     BrowserModule,
